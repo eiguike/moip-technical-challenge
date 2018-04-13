4af4eef680ab33308f6ac37fac6dbcb3ee50edb5
Henrique <rick.eihara@gmail.com>
2018-04-13 00:38:16 -0300

    test: create tests to validate uniquness and presence of name, cpf and email for Buyer model

--- app/models/buyer.rb
+++ app/models/buyer.rb
@@ -1,2 +1,4 @@
 class Buyer < ApplicationRecord
+  validates_presence_of :name, :email, :CPF
+  validates :email, :CPF, uniqueness: true
 end
---
+++ spec/models/buyer_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe Buyer, type: :model do
+  describe "Validation" do
+    it "is not valid when name is empty" do
+      buyer = Buyer.new(:email => "teste@gmail.com", :CPF => "111.111.111-11")
+      expect(buyer).to_not be_valid
+    end
+    it "is not valid when email is empty" do
+      buyer = Buyer.new(:name => "João", :CPF => "111.111.111-11")
+      expect(buyer).to_not be_valid
+    end
+    it "is not valid when the email registered already exists" do
+      buyer = Buyer.create(:name => "João", :email => "teste@gmail.com", :CPF => "111.111.111-11")
+      buyer = Buyer.create(:name => "João", :email => "teste@gmail.com", :CPF => "222.222.222-22")
+      expect(buyer).to_not be_valid
+    end
+    it "is not valid when CPF is empty" do
+      buyer = Buyer.new(:name => "João", :email => "teste@gmail.com")
+      expect(buyer).to_not be_valid
+    end
+    it "is not valid when the CPF registered already exists" do
+      buyer = Buyer.create(:name => "João", :email => "teste@gmail.com", :CPF => "111.111.111-11")
+      buyer = Buyer.new(:name => "João", :email => "teste2@gmail.com", :CPF => "111.111.111-11")
+      expect(buyer).to_not be_valid
+    end
+  end
+end
