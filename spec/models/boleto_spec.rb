e4613c694df55bfc9dc7308857b727186789ff9a
Henrique <rick.eihara@gmail.com>
2018-04-13 20:27:32 -0300

    test: create validations to check presence of boleto's number and its uniqueness

--- spec/models/boleto_spec.rb
+++ spec/models/boleto_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Boleto, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Validations" do
+    it "is not valid when number is empty" do
+      boleto = Boleto.new
+      expect(boleto).to_not be_valid
+    end
+    it "is not valid when the boleto's number is already registered" do
+      boleto = Boleto.create(:number => "00190000090302122300737724708179780700000111360")
+      boleto = Boleto.new(:number => "00190000090302122300737724708179780700000111360")
+      expect(boleto).to_not be_valid
+    end
+  end
 end
