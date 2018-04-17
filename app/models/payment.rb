c77d349ae08dfb4e98eefbedbd3eeda98a32b2c6
Henrique <rick.eihara@gmail.com>
2018-04-17 20:10:29 -0300

    feat: add new validation to status's payment into model

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -3,5 +3,9 @@ class Payment < ApplicationRecord
   belongs_to :buyer
   belongs_to :method, polymorphic: true
 
+  validates :status,
+    format: {with: /(^success$|^fail$|^$)/,
+    message: "only allow 'success' or 'fail'"},
+    presence: true
   validates_presence_of :amount
 end
