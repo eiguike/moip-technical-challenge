f93d5a9a4223667ede65ae61780c732264bd9751
Henrique <rick.eihara@gmail.com>
2018-04-17 19:52:41 -0300

    feat: add payment status (sucess, fail)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_13_232926) do
+ActiveRecord::Schema.define(version: 2018_04_17_224937) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 2018_04_13_232926) do
     t.integer "method_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
     t.index ["buyer_id"], name: "index_payments_on_buyer_id"
     t.index ["client_id"], name: "index_payments_on_client_id"
   end
