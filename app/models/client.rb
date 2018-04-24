d4a79c54b483e85d0bba18cae4c76ccdf958465c
Henrique <rick.eihara@gmail.com>
2018-04-24 20:41:54 -0300

    refact/test: add new client validations, create new unit tests

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,3 +1,5 @@
 class Client < ApplicationRecord
   validates_presence_of :name
+
+  has_many :payments
 end
--- spec/models/client_spec.rb
+++ spec/models/client_spec.rb
@@ -6,5 +6,7 @@ RSpec.describe Client, type: :model do
       client = Client.new
       expect(client).to_not be_valid
     end
+
+    it { should have_many(:payments) }
   end
 end
