382cadc18e1fb0b41deb215494b9a8a886571946
Henrique <rick.eihara@gmail.com>
2018-04-24 20:37:27 -0300

    refact/test: add new card validations models, create new unit tests to reflect these changes

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,5 +1,18 @@
 class Card < ApplicationRecord
   validates_presence_of :holder_name, :number, :expiration_date, :cvv
 
+  validates :number, format: { with: /\A\d{16}\Z/,
+                               message: "Card's number should have 16 digits"}
+  validates :cvv, format: { with: /\A\d{3}\Z/,
+                            message: "CVV should have 3 digits"}
+  validate :check_expiration_date
+
   has_many :payments, as: :method
+
+  def check_expiration_date
+    return true unless expiration_date != nil
+    if Time.now > expiration_date
+      errors.add(:expiration_date, "Card already expired")
+    end
+  end
 end
--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Card, type: :model do
   describe "Validations" do
     it "is not valid when holder_name is empty" do
-      card = Card.new(:number => "49295463632986724485835022353606448544825998158445327957980420934539500792751423",
+      card = Card.new(:number => Faker::Number.number(16),
                       :expiration_date => Time.now,
                       :cvv => 111)
       expect(card).not_to be_valid
@@ -15,16 +15,58 @@ RSpec.describe Card, type: :model do
       expect(card).not_to be_valid
     end
     it "is not valid when expiration_date is empty" do
-      card = Card.new(:number => "49295463632986724485835022353606448544825998158445327957980420934539500792751423",
+      card = Card.new(:number => Faker::Number.number(16),
                       :holder_name=> "João",
                       :cvv => 111)
       expect(card).not_to be_valid
     end
     it "is not valid when cvv is empty" do
-      card = Card.new(:number => "49295463632986724485835022353606448544825998158445327957980420934539500792751423",
+      card = Card.new(:number => Faker::Number.number(16),
                       :holder_name=> "João",
                       :expiration_date => Time.now)
       expect(card).not_to be_valid
     end
+
+    it "is not valid when the card already expired" do
+      card = Card.new(:number => Faker::Number.number(16),
+                      :holder_name=> "João",
+                      :expiration_date => Time.now - 1.year,
+                      :cvv => 111)
+      expect(card).not_to be_valid
+    end
+
+    it "is not valid when the card is invalid" do
+      card = Card.new(:number => Faker::Number.number(17),
+                      :holder_name=> "João",
+                      :expiration_date => Time.now - 1.year,
+                      :cvv => 111)
+      expect(card).not_to be_valid
+    end
+
+    it "is not valid when the cvv is invalid (with letters)" do
+      card = Card.new(:number => Faker::Number.number(17),
+                      :holder_name=> "João",
+                      :expiration_date => Time.now - 1.year,
+                      :cvv => "asdf")
+      expect(card).not_to be_valid
+    end
+
+    it "is not valid when the cvv is invalid" do
+      card = Card.new(:number => Faker::Number.number(17),
+                      :holder_name=> "João",
+                      :expiration_date => Time.now - 1.year,
+                      :cvv => "1234")
+      expect(card).not_to be_valid
+    end
+
+    it "is valid when everything is fullfil" do
+      card = Card.new(:number => Faker::Number.number(16),
+                      :holder_name=> "João",
+                      :expiration_date => Time.now + 1.year,
+                      :cvv => 111)
+      expect(card).to be_valid
+    end
+
+    it { should have_many(:payments) }
   end
 end
