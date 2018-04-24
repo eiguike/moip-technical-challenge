c20d82c40cfe9c65811bcc77559748239e03b461
Henrique <rick.eihara@gmail.com>
2018-04-24 20:01:32 -0300

    test/refact: add new validations in boleto's models, create unit tests for them

--- app/models/boleto.rb
+++ app/models/boleto.rb
@@ -1,6 +1,10 @@
 class Boleto < ApplicationRecord
   validates_presence_of :number
-  validates :number, uniqueness: true
+  validates :number, uniqueness: true,
+                     format: { with: /\A\d{47}\Z/,
+                               message: "Boleto's number should have 47 digits" },
+                     length: { is: 47 }
+
 
   has_many :payments, as: :method
 end
--- spec/models/boleto_spec.rb
+++ spec/models/boleto_spec.rb
@@ -6,10 +6,23 @@ RSpec.describe Boleto, type: :model do
       boleto = Boleto.new
       expect(boleto).to_not be_valid
     end
+
     it "is not valid when the boleto's number is already registered" do
       boleto = Boleto.create(:number => "00190000090302122300737724708179780700000111360")
       boleto = Boleto.new(:number => "00190000090302122300737724708179780700000111360")
       expect(boleto).to_not be_valid
     end
+
+    it "is not valid when Boleto's number has some letters" do
+      boleto = Boleto.new(:number => "a0190000090302122300737724708179780700000111360")
+      expect(boleto).to_not be_valid
+    end
+
+    it "is valid when Boleto has 26 digits" do
+      boleto = Boleto.new(:number => "00190000090302122300737724708179780700000111360")
+      expect(boleto).to be_valid
+    end
+
+    it { should have_many(:payments) }
   end
 end
