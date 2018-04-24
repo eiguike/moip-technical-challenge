2504d359e4d4332a0befb6bebd2688589c403b6f
Henrique <rick.eihara@gmail.com>
2018-04-24 19:03:32 -0300

    refact: validation in schema when a column is null

--- db/migrate/20180413232926_create_payments.rb
+++ db/migrate/20180413232926_create_payments.rb
@@ -2,8 +2,8 @@ class CreatePayments < ActiveRecord::Migration[5.2]
   def change
     create_table :payments do |t|
       t.float :amount
-      t.references :client, foreign_key: true
-      t.references :buyer, foreign_key: true
+      t.references :client, foreign_key: true, null: false
+      t.references :buyer, foreign_key: true, null: false
       t.string :method_type
       t.integer :method_id
 
